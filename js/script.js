fd24bfa5e53e7db0b34654082dc189a9860a88a7
omskscream <golovkov93@gmail.com>
2014-09-01 22:38:53 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    var btn = document.getElementById('showBtn');
+    var dogeDiv = document.getElementById('dogeDiv');
+    var dogeIsShown = false;
+    btn.onclick = function() {
+        if (!dogeIsShown) {
+            btn.innerHTML = 'Hide doge';
+            dogeDiv.style.display = 'block';
+            dogeIsShown = !dogeIsShown;
+        } else {
+            btn.innerHTML = 'Show doge';
+            dogeDiv.style.display = 'none';
+            dogeIsShown = !dogeIsShown;
+        }
+    }
+}
